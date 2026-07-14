49fd65fc4952476e2544993e4585af2d88b0b09b
aln-f <144757715+aln-f@users.noreply.github.com>
2026-07-14 18:06:01 -0400

    Filter users by Last Name

--- routes/users.js
+++ routes/users.js
@@ -36,6 +36,12 @@ router.get("/:email", (req, res) => {
     res.send(filtered_users);
 });
 
+// GET request: Retrieve all users with a particular Last Name
+router.get("/lastName/:lastName", (req, res) => {
+    const lastName = req.params.lastName;
+    let filtered_users = users.filter((user) => user.lastName === lastName);
+    res.send(filtered_users);
+});
 
 // POST request: Create a new user
 router.post("/", (req, res) => {
